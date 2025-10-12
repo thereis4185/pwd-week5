9a7721a424438ca0254646d327e1164bf0649b08
LEE CHANHO <chanho4185@gmail.com>
2025-10-12 22:37:13 +0900

    Init pwd-week5 : Express

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const mongoose = require('mongoose');
 function createApp() {
   const app = express();
 
-  app.use(cors());
+  app.use(cors({
+    origin:'*',
+  }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
